af924f6cf600080b62c987df585f26f8029ac4f3
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-09-06 20:31:48 +0530

    promises and promise.all vs callback functions

--- sharpener_Adv_JS.js
+++ sharpener_Adv_JS.js
@@ -1,22 +1,82 @@
-let student={age:20};
-let addAgePlus=function(a,c){
-	return this.age+a+c;
-};
 
-let b=[5,2];
-// call--functionname.call(obj,arg1,arg2)
-console.log(addAgePlus.call(student,5,3));
 
-// apply--functionName.apply(obj,[arg1,arg2])
-console.log(addAgePlus.apply(student,b));
+const posts=[
+    {title:'Post One',body:'This is post one',createdAt:new Date().getTime()},
+    {title:'Post two',body:'This is post two',createdAt:new Date().getTime()},
+    {title:'Post three',body:'This is post three',createdAt:new Date().getTime()},
+];
+let timeInterval;
+function getPost(){
+    clearInterval=timeInterval;
+    return new Promise(()=>{
+        timeInterval=setInterval(() =>{
+            let output='';
+            posts.forEach((post,index) => {
+                output +=`<li>${post.title} created ${(new Date().getTime()-post.createdAt)/1000} seconds ago</li>`
+            });
+            document.body.innerHTML=output;
+        },1000);
+    });
+}
+// function creatPost(post,callback){
+//     setTimeout(()=>{
+//         posts.push({...post,createdAt:new Date().getTime()});
+//         callback();
+//     },2000);
+// }
+// function creat4thPost(post,callback){
+//     setTimeout(()=>{
+//         posts.push({...post,createdAt:new Date().getTime()});
+//         callback();
+//     },6000);
+// }
+// creatPost({title:'Post three',body:'This is post three'},getPost);
+// creat4thPost({title:'Post four',body:'This is post four'},getPost);
 
-// Bind---bound=functionName.bind(obj)
-// bound(arg1,arg2);
-let pack=addAgePlus.bind(student);
-console.log(pack(5,4));
-// currying
-let addAge=function(a,c){
-	return a+c;
-};
-let package=addAge.bind(this, student.age,5);
-console.log(package());
+function delPost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.pop();
+              
+            if(posts.length!=0){
+                resolve();
+                updateLastUserActivityTime();
+            }
+            else{
+                reject("Array is empty")
+            }      
+             
+        },1000);
+    });    
+}
+
+function creat4thPost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.push({...post,createdAt:new Date().getTime()});
+            resolve();
+            updateLastUserActivityTime();
+        },1000);
+    });
+}   
+
+
+function updateLastUserActivityTime(){
+    clearInterval=timeInterval;
+    return new Promise(()=>{
+        timeInterval=setInterval(() =>{
+            let output='';
+            posts.forEach((post,index) => {
+                output +=`<li>${post.title} created ${(new Date().getTime()-post.createdAt)/1000} seconds ago</li>`
+            });
+            output +=`Last updated at:-- ${(new Date())}`; 
+
+            document.body.innerHTML=output;
+            // document.body.innerHTML=output;
+        },1000);
+    });
+}  
+
+// creat4thPost({title:'Post four',body:'This is post four'}).then(updateLastUserActivityTime);
+Promise.all([creat4thPost({title:'Post four',body:'This is post four'}), updateLastUserActivityTime(), delPost()])
+// updateLastUserActivityTime();
